9318be550ece79137afe39c3a5d565820cb88980
cuongnm265 <cuongnm265@gmail.com>
2017-06-23 11:04:04 +0700

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,11 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
- 
+var templateData = require('./data/newsletter.json');
+
 gulp.task('make', function () {
-    var templateData = {
-        firstName: 'Eugene'
-    },
-    options = {
-        batch : ['./src/partials'],
-        helpers : {
-            capitals : function(str){
-                return str.toUpperCase();
-            }
-        }
-    }
- 
     return gulp.src('src/body.handlebars')
-        .pipe(handlebars(templateData, options))
+        .pipe(handlebars(templateData))
         .pipe(rename('newsletter.html'))
         .pipe(gulp.dest('dist'));
 });
